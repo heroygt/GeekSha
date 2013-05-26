1b8f447c7c555c5ba9553fee9280c5991771fac9
Isaac <isaachanstar@gmail.com>
2013-05-27 07:05:05 +0800

    keep remaining cards when deal done.

--- game.pl
+++ game.pl
@@ -1,8 +1,7 @@
-game(Players, Cards) :-
-    deal(Players, Cards, PlayersWithCards).
+game(InitPlayers, Cards) :-
+    deal(InitPlayers, Cards, Players, RemainingCards).
 
+deal([], Cards, [], Cards).
+deal([P1|Players], [C1,C2|Cards], [{P1, [C1,C2]}|Other], RemainingCards) :-
+    deal(Players, Cards, Other, RemainingCards).
 
-deal([], _, []).
-
-deal([FirstPlayer|OtherPlayers], [FirstCard, SecondCard|OtherCards], [{FirstPlayer, [FirstCard, SecondCard]}|Other]) :-
-    deal(OtherPlayers, OtherCards, Other).
--- test.pl
+++ test.pl
@@ -3,6 +3,7 @@
 :- Players = [zhang3, li4, wang5], 
    Cards = [kill, hide, health, hide, kill, health, kill, kill],
    
-   deal(Players, Cards, Result),  
+   deal(Players, Cards, Result, RemainingCards),  
     
-   Result = [{zhang3, [kill, hide]}, {li4, [health, hide]}, {wang5, [kill, health]}].
+   Result = [{zhang3, [kill, hide]}, {li4, [health, hide]}, {wang5, [kill, health]}],
+   RemainingCards = [kill, kill].
